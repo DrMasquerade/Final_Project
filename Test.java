713023ca3c11ae8425d3c41393eee35edc788e91
fiscc8442 <da>
2019-05-29 09:14:42 -0400

    Does this finally Work

---
+++ Test.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) { //Main function good to add the scanner just in case
+        Scanner sin = new Scanner(System.in);
+    }
+}
