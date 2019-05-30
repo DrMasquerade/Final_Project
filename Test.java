baed49db48b3ffdc20059bfa5dc4c0fb3fa73bee
Dragon7833 <50936243+Dragon7833@users.noreply.github.com>
2019-05-30 08:34:08 -0400

    Delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Test {
-    public static void main(String[] args) { //Main function good to add the scanner just in case
-        Scanner sin = new Scanner(System.in);
-    }
-}
